35faa4bedc6389da83ef86199b63b0862c285548
Mike Talley <miketalley@hotmail.com>
2016-09-13 08:18:15 -0400

    Added template for nav

--- app/main.js
+++ app/main.js
@@ -5,7 +5,8 @@ requirejs.config({
     plugins : '../node_modules/durandal/js/plugins',
     transitions : '../node_modules/durandal/js/transitions',
     knockout: '../node_modules/knockout/build/output/knockout-latest',
-    jquery: '../node_modules/jquery/dist/jquery'
+    jquery: '../node_modules/jquery/dist/jquery',
+    foundation: '../node_modules/foundation-sites/dist/foundation.min.js'
   }
 });
 
---
+++ app/templates/nav.js
@@ -0,0 +1,10 @@
+define([], function() {
+  'use strict';
+
+  function Nav() {
+    var self = this;
+  }
+
+  return Nav;
+
+});
